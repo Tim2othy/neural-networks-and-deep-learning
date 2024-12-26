7cea3427b66fcb15b892018ea4c4a8a0fbb2f55b
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-12-26 21:25:56 +0100

    removed.vscode

--- My_code/C1digits/start.py
+++ My_code/C1digits/start.py
@@ -8,7 +8,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 net = network.Network([784, 46, 24, 10])
 
-net.SGD(training_data, 5, 8, 3.0, test_data=test_data)
+net.SGD(training_data, 1, 8, 3.0, test_data=test_data)
 
 
 print("end of start")
