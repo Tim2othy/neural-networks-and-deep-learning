e42b23d65f9e4ab64dc0d9f408eed24c78b66394
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-12-26 21:33:38 +0100

    added files for chapter 3 programm

---
+++ My_code/C2cross-entropy/start2.py
@@ -0,0 +1,16 @@
+# starting_ch1.py
+
+import mnist_loader2
+import network2
+
+
+training_data, validation_data, test_data = mnist_loader2.load_data_wrapper()
+
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+
+net.large_weight_initializer()
+
+net.SGD(training_data, 1, 8, 3.0, test_data=test_data)
+
+
+print("end of start")
