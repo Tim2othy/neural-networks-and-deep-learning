9867ea3268ab5f7b9cf1f2a8a107bc5e31ced006
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-12-26 21:14:05 +0100

    ran network on mnst a couple mor times

--- NNADL or so/starting_ch1.py
+++ NNADL or so/starting_ch1.py
@@ -6,10 +6,9 @@ import network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-net = network.Network([784, 10])
+net = network.Network([784, 46, 24, 10])
 
-net.SGD(training_data, 20, 5, 2.0, test_data=test_data)
+net.SGD(training_data, 5, 8, 3.0, test_data=test_data)
 
 
-
-print('end of starting')
+print("end of starting")
