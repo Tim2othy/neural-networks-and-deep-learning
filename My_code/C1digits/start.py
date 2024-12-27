b398c5c0923e1f33de0e914f582f394a1909ac0e
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-12-27 02:54:36 +0100

    cleaned up code of start and start2 slightly

--- My_code/C1digits/start.py
+++ My_code/C1digits/start.py
@@ -3,12 +3,16 @@
 import mnist_loader
 import network
 
-
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-net = network.Network([784, 46, 24, 10])
+net = network.Network([784, 32, 10])
 
-net.SGD(training_data, 1, 8, 3.0, test_data=test_data)
-
+net.SGD(
+    training_data,
+    1,
+    8,
+    3.0,
+    test_data=test_data
+)
 
 print("end of start")
--- My_code/C3cross-entropy/start2.py
+++ My_code/C3cross-entropy/start2.py
@@ -2,22 +2,20 @@
 
 import mnist_loader2
 import network2
-import network
 
 training_data, validation_data, test_data = mnist_loader2.load_data_wrapper()
 
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net = network2.Network([784, 32, 10], cost=network2.CrossEntropyCost)
 
 net.large_weight_initializer()
 
 net.SGD(
     training_data,
-    3,
+    1,
     8,
-    3.0,
+    0.5,
     evaluation_data=test_data,
     monitor_evaluation_accuracy=True,
 )
-
 
 print("end of start2")
