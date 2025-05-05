a68c9074e08b2b9fe49dc239444c5be3b857a02c
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-05-05 18:45:41 +0200

    fix up C3 network and start

--- My_code/C3cross-entropy/start2.py
+++ My_code/C3cross-entropy/start2.py
@@ -1,21 +1,19 @@
 # starting_ch1.py
 
-import mnist_loader2
-import network2
+from mnist_loader import load_data_wrapper
+from network2 import CrossEntropyCost, Network
 
-training_data, validation_data, test_data = mnist_loader2.load_data_wrapper()
+training_data, validation_data, test_data = load_data_wrapper()
 
-net = network2.Network([784, 32, 10], cost=network2.CrossEntropyCost)
+net = Network([784, 32, 10], cost=CrossEntropyCost)
 
 net.large_weight_initializer()
 
 net.SGD(
     training_data,
-    1,
+    3,
     8,
     0.5,
     evaluation_data=test_data,
     monitor_evaluation_accuracy=True,
 )
-
-print("end of start2")
